752b7c7923e05d97a445c68490eb388d5115de99
Daniyal Rafiq <daniyal.smtek@gmail.com>
2025-09-24 19:34:39 +0500

    task check

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,7 @@ Route::prefix('v2')->group(function () {
         Route::prefix('custome')->group(function () {
             Route::controller(CustomeUniformController::class)->name('custome.')->group(function () {
                 Route::get('soccer', 'soccer')->name('index');   // /v2/backend/custome/index
+                Route::get('view', 'view')->name('view');   // /v2/backend/custome/index
                 Route::get('circket', 'circket')->name('circket');   // /v2/backend/custome/index
                 Route::post('store', 'store')->name('store');  // /v2/backend/custome/store
                 Route::post('cart/clear', 'clearCart')->name('cart.clear');  // /v2/backend/custome/store
@@ -81,7 +82,6 @@ Route::prefix('v2')->group(function () {
             Route::controller(OrderController::class)->name('order.')->group(function () {
                 Route::get('create', 'create')->name('create');   // /v2/backend/custome/index
                 Route::post('store', 'store')->name('store');   // /v2/backend/custome/index
-
             });
         });
 
